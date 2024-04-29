812c34b4e60baa94bfc45f8f23d8a2785338d849
minh-le0205 <minh.levunguyen@gmail.com>
2024-04-29 21:28:38 +0700

    Update layout

---
+++ resources/views/admin/elements/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- footer -->
+<footer>
+    <div class="pull-right">
+        Học lập trình online tại <a href="https://colorlib.com/">ZendVN.com</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer -->
