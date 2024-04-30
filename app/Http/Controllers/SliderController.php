3f6dedf84b8a4ee5d095d3bc87ced256eb85181d
minh-le0205 <minh.levunguyen@gmail.com>
2024-04-30 15:11:11 +0700

    add images

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\SliderModel;
 
 
 class SliderController extends Controller
