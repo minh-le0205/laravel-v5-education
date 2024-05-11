e9976bc6f75f733ba2fa30f8b718c8ff81d7e142
minh-le0205 <minh.levunguyen@gmail.com>
2024-05-11 10:23:16 +0700

    Update table image

--- resources/views/admin/pages/slider/list.blade.php
+++ resources/views/admin/pages/slider/list.blade.php
@@ -3,22 +3,27 @@
         <table class="table table-striped jambo_table bulk_action">
             <thead>
                 <tr class="headings">
-                    <th class="column-title">ID</th>
-                    <th class="column-title">Slider Info</th>
-                    <th class="column-title">Trạng thái</th>
-                    <th class="column-title">Tạo mới</th>
-                    <th class="column-title">Chỉnh sửa</th>
-                    <th class="column-title">Hành động</th>
+                    <th class="column-title text-center">ID</th>
+                    <th class="column-title text-center">Slider Name</th>
+                    <th class="column-title text-center">Image</th>
+                    <th class="column-title text-center">Trạng thái</th>
+                    <th class="column-title text-center">Tạo mới</th>
+                    <th class="column-title text-center">Chỉnh sửa</th>
+                    <th class="column-title text-center">Hành động</th>
                 </tr>
             </thead>
             <tbody>
                 @if (count($items) > 0)
                     @foreach ($items as $item)
                         <tr class="even pointer">
-                            <td class="">{{ $item->id }}</td>
-                            <td class="">{{ $item->name }}</td>
+                            <td class="text-center">{{ $item->id }}</td>
+                            <td class="text-center">{{ $item->name }}</td>
+                            <td class="text-center" style="max-width: 30em">
+                                <img style="vertical-align: bottom;" width="100%" height="100%"
+                                    src="{{ asset("admin/images/sliders/$item->thumb") }}" alt="">
                             </td>
-                            <td><a href="/change-status-active/{{ $item->id }}" type="button"
+                            </td>
+                            <td class="text-center"><a href="/change-status-active/{{ $item->id }}" type="button"
                                     class="btn btn-round btn-success">{{ $item->status }}</a>
                             </td>
                             <td>
