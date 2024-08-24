5e63e2d653e95cfc9933abaf46e74d93dbda6e74
minh-le0205 <minh.levunguyen@gmail.com>
2024-08-24 17:37:20 +0700

    Update pagianation still keeping param

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -18,7 +18,7 @@ class SliderController extends Controller
     public function __construct()
     {
         $this->model = new MainModel();
-        $this->params['pagination']['totalItemsPerPage'] = 5;
+        $this->params['pagination']['totalItemsPerPage'] = 1;
         view()->share('controllerName', $this->controllerName);
     }
     public function index(Request $request)
