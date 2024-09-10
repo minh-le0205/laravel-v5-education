68e0f6040d94a5c38e6f4b94efa1e78adb79350f
minh-le0205 <minh.levunguyen@gmail.com>
2024-09-10 21:55:41 +0700

    Update icon

--- resources/views/admin/elements/sidebar_menu.blade.php
+++ resources/views/admin/elements/sidebar_menu.blade.php
@@ -15,7 +15,7 @@
       <li><a href="{{ route('dashboard') }}"><i class="fa fa-home"></i> Home</a></li>
       <li><a href="{{ route('slider') }}"><i class="fa fa-sliders"></i> Silders</a></li>
       <li><a href="{{ route('category') }}"><i class="fa fa fa-building-o"></i> Categories</a></li>
-      <li><a href="{{ route('article') }}"><i class="fa fa fa-building-o"></i> Articles</a></li>
+      <li><a href="{{ route('article') }}"><i class="fa fa-newspaper-o"></i> Articles</a></li>
     </ul>
   </div>
 </div>
