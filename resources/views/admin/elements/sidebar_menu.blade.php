b0c05de39f872f343a2f804319e39cd04c9d0e08
minh-le0205 <minh.levunguyen@gmail.com>
2024-09-24 21:46:53 +0700

    Update menu

--- resources/views/admin/elements/sidebar_menu.blade.php
+++ resources/views/admin/elements/sidebar_menu.blade.php
@@ -12,7 +12,7 @@
     <div class="menu_section">
         <h3>General</h3>
         <ul class="nav side-menu">
-            <li><a href="{{ route('dashboard') }}"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-home"></i> Dashboard</a></li>
             <li><a href="{{ route('slider') }}"><i class="fa fa-sliders"></i> Silders</a></li>
             <li><a href="{{ route('user') }}"><i class="fa fa-user"></i> Users</a></li>
             <li><a href="{{ route('category') }}"><i class="fa fa fa-building-o"></i> Categories</a></li>
