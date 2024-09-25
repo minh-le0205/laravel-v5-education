9650e45f6d348fc302594bbb2266e385c9af52b3
minh-le0205 <minh.levunguyen@gmail.com>
2024-09-25 20:49:20 +0700

    Update dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Admin\AdminController;
-
+use App\Models\CategoryModel;
+use App\Models\ArticleModel;
+use App\Models\SliderModel;
+use App\Models\UserModel;
 
 class DashboardController extends AdminController
 {
-  // private $pathViewController = "admin.pages.dashboard.";
-  // private $controllerName = 'dashboard';
-
   public function __construct()
   {
     $this->pathViewController = "admin.pages.dashboard.";
@@ -21,6 +21,35 @@ class DashboardController extends AdminController
 
   public function dashboard()
   {
-    return view($this->pathViewController . "index");
+    $dashboardData = [
+      [
+        'text' => 'Tổng số danh mục',
+        'count' => CategoryModel::count(),
+        'link' => route('category'),
+        'icon' => 'fa fa-table'
+      ],
+      [
+        'text' => 'Tổng số bài viết',
+        'count' => ArticleModel::count(),
+        'link' => route('article'),
+        'icon' => 'fa fa-newspaper-o'
+      ],
+      [
+        'text' => 'Tổng số người dùng',
+        'count' => UserModel::count(),
+        'link' => route('user'),
+        'icon' => 'fa fa-user'
+      ],
+      [
+        'text' => 'Tổng số slider',
+        'count' => SliderModel::count(),
+        'link' => route('slider'),
+        'icon' => 'fa fa-sliders'
+      ]
+    ];
+
+    return view($this->pathViewController . "index", [
+      'dashboardData' => $dashboardData
+    ]);
   }
 }
--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -7,38 +7,17 @@
     </div>
     <div class="row" style="display: inline-block;">
         <div class="top_tiles">
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                <div class="tile-stats">
-                    <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
-                    <div class="count">179</div>
-                    <h3>New Sign ups</h3>
-                    <p>Lorem ipsum psdea itgum rixt.</p>
+            @foreach ($dashboardData as $item)
+                <div class="col-md-6" style="width:250px">
+                    <div class="tile-stats">
+                        <div class="icon"><i class="{!! $item['icon'] !!}"></i></div>
+                        <div class="count">{!! $item['count'] !!}</div>
+                        <h4 style="padding-left:5%;">{!! $item['text'] !!}</h4>
+                        <a style="padding-left:66%;" href="{!! $item['link'] !!}">Xem chi tiết</a>
+                    </div>
                 </div>
-            </div>
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                <div class="tile-stats">
-                    <div class="icon"><i class="fa fa-comments-o"></i></div>
-                    <div class="count">179</div>
-                    <h3>New Sign ups</h3>
-                    <p>Lorem ipsum psdea itgum rixt.</p>
-                </div>
-            </div>
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                <div class="tile-stats">
-                    <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
-                    <div class="count">179</div>
-                    <h3>New Sign ups</h3>
-                    <p>Lorem ipsum psdea itgum rixt.</p>
-                </div>
-            </div>
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                <div class="tile-stats">
-                    <div class="icon"><i class="fa fa-check-square-o"></i></div>
-                    <div class="count">179</div>
-                    <h3>New Sign ups</h3>
-                    <p>Lorem ipsum psdea itgum rixt.</p>
-                </div>
-            </div>
+            @endforeach
+
         </div>
     </div>
 @endsection
