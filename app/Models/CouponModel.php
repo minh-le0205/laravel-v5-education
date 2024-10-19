265b77f834c412c576971a02cc958dbbeefc9ad1
minh-le0205 <minh.levunguyen@gmail.com>
2024-10-19 21:11:59 +0700

    Init Coupon controller, model, request, routes

---
+++ app/Models/CouponModel.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\AdminModel;
+use DB;
+
+class CouponModel extends AdminModel
+{
+    public function __construct()
+    {
+        $this->table = 'coupon';
+        $this->folderUpload = 'coupon';
+        $this->fieldSearchAccepted = [
+            'id',
+            'code',
+            'type',
+            'value',
+            'status'
+        ];
+        $this->crudNotAccepted = [
+            '_token'
+        ];
+    }
+
+    public function getListItems($params, $options)
+    {
+        $result = null;
+
+        if ($options['task'] == "admin-list-item") {
+            $query = $this->select('*');
+
+            if ($params['filter']['status'] !== "all") {
+                $query->where('status', '=', $params['filter']['status']);
+            }
+
+            if ($params['search']['value'] !== "") {
+                if ($params['search']['field'] == "all") {
+                    $query->where(function ($query) use ($params) {
+                        foreach ($this->fieldSearchAccepted as $column) {
+                            $query->orWhere($column, 'LIKE',  "%{$params['search']['value']}%");
+                        }
+                    });
+                } else if (in_array($params['search']['field'], $this->fieldSearchAccepted)) {
+                    $query->where($params['search']['field'], 'LIKE',  "%{$params['search']['value']}%");
+                }
+            }
+
+            $result =  $query->orderBy('ordering', 'asc')
+                ->paginate($params['pagination']['totalItemsPerPage']);
+        }
+
+        if ($options['task'] == 'news-list-items') {
+            $query = $this->select('*')
+                ->where('status', '=', 'active')
+                ->orderBy('ordering', 'asc');
+
+            $result = $query->get()->toArray();
+        }
+
+
+
+        return $result;
+    }
+
+    public function getItem($params, $options)
+    {
+        $result = null;
+
+        if ($options['task'] == 'get-item') {
+            $result = self::select('*')->where('id', $params['id'])->first();
+        }
+
+        return $result;
+    }
+
+    public function countListItems($params, $options)
+    {
+        $result = null;
+
+        if ($options['task'] == 'admin-list-item') {
+
+            $query = $this::groupBy('status')
+                ->select(DB::raw('count(*) as user_count, status'));
+
+            if ($params['search']['value'] !== "") {
+                if ($params['search']['field'] == "all") {
+                    $query->where(function ($query) use ($params) {
+                        foreach ($this->fieldSearchAccepted as $column) {
+                            $query->orWhere($column, 'LIKE',  "%{$params['search']['value']}%");
+                        }
+                    });
+                } else if (in_array($params['search']['field'], $this->fieldSearchAccepted)) {
+                    $query->where($params['search']['field'], 'LIKE',  "%{$params['search']['value']}%");
+                }
+            }
+
+            $result = $query->get()->toArray();
+        }
+
+        return $result;
+    }
+
+    public function saveItem($params, $options)
+    {
+        if ($options['task'] == 'change-status') {
+            $status = ($params['currentStatus'] == "active") ? "inactive" : "active";
+            self::where('id', $params['id'])->update(['status' => $status]);
+        }
+
+        if ($options['task'] == 'add-item') {
+            $params['created_by'] = session('userInfo')['username'];
+            $params['created'] = date('Y-m-d H:i:s');
+            self::insert($this->prepareParams($params));
+        }
+
+        if ($options['task'] == 'edit-item') {
+            $params['modified_by'] = session('userInfo')['username'];
+            $params['modified'] = date('Y-m-d H:i:s');
+            self::where('id', $params['id'])->update($this->prepareParams($params));
+        }
+    }
+
+    public function deleteItem($params, $options)
+    {
+        if ($options['task'] == 'delete-item') {
+            self::where('id', $params['id'])->delete();
+        }
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -435,4 +435,35 @@ Route::group(['prefix' => $prefixAdmin, 'namespace' => 'Admin', 'middleware' => 
       'uses' => $controller . 'media'
     ]);
   });
+
+
+  // Coupon Group
+  $prefix = 'coupon';
+  $controllerName = 'coupon';
+  Route::group(['prefix' => $prefix], function () use ($controllerName) {
+    $controller = ucfirst($controllerName) . 'Controller@';
+    Route::get('/', [
+      'as' => $controllerName,
+      'uses' => $controller . 'index'
+    ]);
+    Route::get('/form/{id?}', [
+      'as' => $controllerName . '/form',
+      'uses' => $controller . 'formProduct'
+    ])->where('id', '[0-9]+');
+
+    Route::post('/save', [
+      'as' => $controllerName . '/save',
+      'uses' => $controller . 'save'
+    ]);
+
+    Route::get('/delete/{id}', [
+      'as' => $controllerName . '/delete',
+      'uses' => $controller . 'delete'
+    ])->where('id', '[0-9]+');
+
+    Route::get('/change-status-{status}/{id}', [
+      'as' => $controllerName . '/status',
+      'uses' => $controller . 'changeStatus'
+    ])->where('id', '[0-9]+');
+  });
 });
